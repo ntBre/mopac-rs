3a13c32d78c0d7b30803237292547c0062e75be8
Brent Westbrook <brentrwestbrook@gmail.com>
2024-11-10 16:14:47 -0500

    add psqs and actually test output

--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,8 @@ include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
 mod tests {
     use std::ffi::CString;
 
+    use psqs::program::{mopac::Mopac, Program};
+
     use super::*;
 
     #[test]
@@ -14,5 +16,11 @@ mod tests {
             run_mopac_from_input(ptr);
             drop(CString::from_raw(ptr));
         }
+
+        let got = Mopac::read_output("testfiles/try").unwrap();
+
+        assert!(got.cart_geom.is_some());
+        assert_eq!(got.cart_geom.as_ref().unwrap().len(), 5);
+        assert_eq!(got.energy, 0.204457543878102);
     }
 }
