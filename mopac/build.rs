637dfe8bc475036dd227ed2b3ffcc6adb9cfec13
Brent Westbrook <brentrwestbrook@gmail.com>
2024-11-16 15:32:54 -0500

    add mopac::System and run SCF

---
+++ mopac/build.rs
@@ -0,0 +1,13 @@
+fn main() {
+    let Ok(mopac_path) = std::env::var("MOPAC_PATH") else {
+        eprintln!("MOPAC_PATH must be set to dir containing build/libmopac.so");
+        std::process::exit(1);
+    };
+
+    println!("cargo:rerun-if-env-changed=MOPAC_PATH");
+
+    // println!("cargo:rustc-link-search={mopac_path}/build");
+    // println!("cargo:rustc-link-lib=mopac");
+
+    println!("cargo:rustc-link-arg=-Wl,-rpath,{mopac_path}/build");
+}
--- mopac/src/lib.rs
+++ mopac/src/lib.rs
@@ -1,18 +1,112 @@
-use mopac_sys::mopac_system;
+use std::{ffi::CStr, mem::MaybeUninit};
 
-pub struct System(mopac_system);
+use mopac_sys::{
+    create_mopac_state, mopac_properties, mopac_scf, mopac_system,
+};
 
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+pub use symm::Molecule;
+
+pub struct System {
+    /// These slices must outlive `system` because `system` contains raw
+    /// pointers to them
+    _atomic_numbers: Box<[i32]>,
+    _coordinates: Box<[f64]>,
+    system: mopac_system,
+}
+
+impl System {
+    pub fn new(mol: Molecule, charge: i32, spin: i32) -> Self {
+        let natoms = mol.atoms.len();
+        let mut atom = {
+            let v: Vec<i32> =
+                mol.atomic_numbers().into_iter().map(|n| n as i32).collect();
+            v.into_boxed_slice()
+        };
+        let mut coord = {
+            let v: Vec<_> = mol.atoms.iter().flat_map(|a| a.coord()).collect();
+            v.into_boxed_slice()
+        };
+        Self {
+            system: mopac_system {
+                natom: natoms as _,
+                natom_move: natoms as _,
+                charge,
+                spin,
+                model: 3,     // PM6
+                epsilon: 1.0, // no solvent
+                atom: atom.as_mut_ptr(),
+                coord: coord.as_mut_ptr(),
+                nlattice: 0,
+                nlattice_move: 0,
+                pressure: 0.0,
+                lattice: std::ptr::null_mut(),
+                tolerance: 1e-8,
+                max_time: 172_800, // default time limit from the docs
+            },
+            _atomic_numbers: atom,
+            _coordinates: coord,
+        }
+    }
+
+    pub fn scf(&mut self) -> Result<Properties, Vec<String>> {
+        let mut state = unsafe {
+            let mut state = MaybeUninit::uninit();
+            create_mopac_state(state.as_mut_ptr());
+            let mut state = state.assume_init();
+            state.mpack = 0;
+            state
+        };
+
+        let props = unsafe {
+            let mut props = MaybeUninit::uninit();
+            mopac_scf(&mut self.system, &mut state, props.as_mut_ptr());
+            props.assume_init()
+        };
+
+        if props.nerror > 0 {
+            let mut ret = Vec::new();
+            for i in 0..props.nerror {
+                let s = unsafe {
+                    CStr::from_ptr(*props.error_msg.offset(i as isize))
+                };
+                ret.push(s.to_string_lossy().to_string());
+            }
+            return Err(ret);
+        }
+
+        Ok(Properties(props))
+    }
+}
+
+pub struct Properties(mopac_properties);
+
+impl Properties {
+    /// Return the final heat of formation in kcal/mol
+    pub fn final_energy(&self) -> f64 {
+        self.0.heat
+    }
 }
 
 #[cfg(test)]
 mod tests {
+    use approx::assert_abs_diff_eq;
+    use symm::molecule;
+
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn c3h2_scf() {
+        let mut mol = molecule! {
+            C      0.000000000000      0.003768239200     -1.686245109400
+            C      0.000000000000      1.243805099800      0.688097726900
+            C      0.000000000000     -1.242134139500      0.700109341300
+            H      0.000000000000      2.998368900600      1.719180578800
+            H      0.000000000000     -3.003808100200      1.718996398400
+        };
+        mol.to_angstrom();
+        let mut system = System::new(mol, 0, 0);
+        let props = system.scf().unwrap();
+
+        assert_abs_diff_eq!(props.final_energy(), 128.29901260, epsilon = 1e-8);
     }
 }
