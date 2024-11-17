be546ae8506f4af47db87f8fc3498d1e02561b13
Brent Westbrook <brentrwestbrook@gmail.com>
2024-11-17 15:27:23 -0500

    add some docs and System::coordinates

--- mopac/src/lib.rs
+++ mopac/src/lib.rs
@@ -1,3 +1,96 @@
+//! A safe Rust interface to the [MOPAC](https://github.com/openmopac/mopac)
+//! chemistry package.
+//!
+//! This package is based on the C API first released in MOPAC
+//! [23.0.0](https://github.com/openmopac/mopac/releases/tag/v23.0.0), which
+//! contains a simple diskless API for running MOPAC calculations. As the
+//! "diskless" part implies, instead of using the normal MOPAC input file,
+//! callers must set up a [mopac_system] in code and invoke one of the
+//! [mopac_scf], [mopac_relax], or [mopac_vibe] functions to obtain the
+//! corresponding [mopac_properties]. Calling these functions directly is
+//! unsafe, so this package attempts to wrap the unsafety in a safe, idiomatic
+//! Rust interface.
+//!
+//! # Examples
+//!
+//! Each of the examples below starts with construction of an input [Molecule].
+//! A [Molecule] is just a sequence of [Atom]s. One of the easiest ways to
+//! construct one is with the [molecule] macro:
+//!
+//! ```
+//! use mopac::molecule;
+//!
+//! let mol = molecule! {
+//!     C      0.000000000000      0.003768239200     -1.686245109400
+//!     C      0.000000000000      1.243805099800      0.688097726900
+//!     C      0.000000000000     -1.242134139500      0.700109341300
+//!     H      0.000000000000      2.998368900600      1.719180578800
+//!     H      0.000000000000     -3.003808100200      1.718996398400
+//! };
+//! ```
+//!
+//! With a [Molecule] in hand, you can construct a [System]. You also need to
+//! specify the `charge` and `spin`, which are often 0:
+//!
+//! ```
+//! # use mopac::{molecule, System};
+//! # let mol = molecule! {
+//! #    C      0.000000000000      0.003768239200     -1.686245109400
+//! #    C      0.000000000000      1.243805099800      0.688097726900
+//! #    C      0.000000000000     -1.242134139500      0.700109341300
+//! #    H      0.000000000000      2.998368900600      1.719180578800
+//! #    H      0.000000000000     -3.003808100200      1.718996398400
+//! # };
+//! let mut system = System::new(mol, 0, 0);
+//! ```
+//!
+//! Note that `mut` is necessary for the method calls coming in the next couple
+//! of sections.
+//!
+//! ## Single-point energies
+//!
+//! From the system, you can compute a single-point energy with the
+//! [System::scf] method and return the final heat of formation in kcal/mol
+//! with [Properties::final_energy]:
+//!
+//! ```
+//! # use mopac::{molecule, System};
+//! # let mol = molecule! {
+//! #    C      0.000000000000      0.001993398537     -0.892023662873
+//! #    C      0.000000000000      0.657972897794      0.364003697530
+//! #    C      0.000000000000     -0.657088959796      0.370357841548
+//! #    H      0.000000000000      1.586137148417      0.909446526185
+//! #    H      0.000000000000     -1.589014485006      0.909349094754
+//! # };
+//! # let mut system = System::new(mol, 0, 0);
+//! let props = system.scf().unwrap();
+//! assert!((props.final_energy() - 128.33088672).abs() < 1e-8);
+//! ```
+//!
+//! ## Geometry optimization
+//!
+//! Similarly, you can run a geometry optimization with [System::optimize] and
+//! retrieve the results with [Properties::coordinates]:
+//!
+//! ```
+//! use mopac::{molecule, System};
+//!
+//! let mol = molecule! {
+//!    C      0.000000000000      0.001993398537     -0.892023662873
+//!    C      0.000000000000      0.657972897794      0.364003697530
+//!    C      0.000000000000     -0.657088959796      0.370357841548
+//!    H      0.000000000000      1.586137148417      0.909446526185
+//!    H      0.000000000000     -1.589014485006      0.909349094754
+//! };
+//! let mut system = System::new(mol, 0, 0);
+//! let props = system.optimize().unwrap();
+//! assert_ne!(props.coordinates(), system.coordinates());
+//! ```
+//!
+//! ## Harmonic frequencies
+//!
+//! TODO
+
 use std::{ffi::CStr, mem::MaybeUninit};
 
 use mopac_sys::{
@@ -5,17 +98,19 @@ use mopac_sys::{
     mopac_system,
 };
 
-pub use symm::Molecule;
+pub use symm::{molecule, Atom, Molecule};
 
 pub struct System {
     /// These slices must outlive `system` because `system` contains raw
     /// pointers to them
     _atomic_numbers: Box<[i32]>,
-    _coordinates: Box<[f64]>,
+    coordinates: Box<[f64]>,
     system: mopac_system,
 }
 
 impl System {
+    /// Construct a MOPAC system from `mol`. The coordinates in `mol` must be in
+    /// Ångstroms.
     pub fn new(mol: Molecule, charge: i32, spin: i32) -> Self {
         let natoms = mol.atoms.len();
         let mut atom = {
@@ -45,7 +140,7 @@ impl System {
                 max_time: 172_800, // default time limit from the docs
             },
             _atomic_numbers: atom,
-            _coordinates: coord,
+            coordinates: coord,
         }
     }
 
@@ -81,6 +176,10 @@ impl System {
         props.check_errors()?;
 
         Ok(props)
+    }
+
+    pub fn coordinates(&self) -> &[f64] {
+        &self.coordinates
     }
 }
 
@@ -183,7 +282,7 @@ mod tests {
         let mut system = System::new(c3h2(), 0, 0);
 
         let props = system.optimize().unwrap();
-        let coords_init = &system._coordinates[..];
+        let coords_init = &system.coordinates[..];
         let coords_final = props.coordinates();
 
         // Make sure some optimization occurred
